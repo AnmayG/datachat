da054172a3368c19975fab35db833e899d44ba99
Heorhii Ambartsumov <gosha20052811@gmail.com>
2025-09-07 07:24:00 -0400

    simple pubsub?

--- src/App.tsx
+++ src/App.tsx
@@ -260,7 +260,7 @@ const AppContent = (props: AppProps) => {
         element={
           !authState.isAuthenticated ? 
             <Navigate to="/login" replace /> : 
-            <HandshakePage />
+            <HandshakePage user={authState.user} />
         } 
       />
       <Route path="*" element={<Navigate to="/" replace />} />
